44f46eccfb5cb993bc6a90ed005537cf4c47adeb
M2 <sahil.kumar@geeky.dev>
2025-03-22 09:45:42 +0530

    removed the github google and other link for now

--- src/Pages/AuthPage/index.tsx
+++ src/Pages/AuthPage/index.tsx
@@ -169,7 +169,7 @@ const AuthPage = ({logged}: any) => {
             </div>
           </form>
 
-          <div className="mt-6">
+          {/* <div className="mt-6">
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300"></div>
@@ -212,7 +212,7 @@ const AuthPage = ({logged}: any) => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
